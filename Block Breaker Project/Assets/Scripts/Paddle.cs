37803ac40d6850f31ad8db4dac2c2fc777e0e6c1
david <davss0802@gmail.com>
2020-05-20 16:39:35 +0400

    Limiting the paddle with screen width

--- Block Breaker Project/Assets/Scripts/Paddle.cs
+++ Block Breaker Project/Assets/Scripts/Paddle.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour
 {
+    [SerializeField] float min = 1f;
+    [SerializeField] float max = 15f;
     [SerializeField] float screenWithInUnits = 16f;
     // Start is called before the first frame update
     void Start()
@@ -15,7 +17,8 @@ public class Paddle : MonoBehaviour
     void Update()
     {
         float mousePosInUnits =    Input.mousePosition.x / Screen.width * screenWithInUnits;
-        Vector2 paddlePos = new Vector2(mousePosInUnits,transform.position.y);
+        Vector2 paddlePos = new Vector2(transform.position.x,transform.position.y);
+        paddlePos.x = Mathf.Clamp(mousePosInUnits,min,max); 
         transform.position = paddlePos;
     }
 }

8b82714560ca71ebbbe093b21b678ff7d7f5db5e
david <davss0802@gmail.com>
2020-05-21 18:46:46 +0400

    Ball Launch

--- Block Breaker Project/Assets/Scripts/Ball.cs
+++ Block Breaker Project/Assets/Scripts/Ball.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
     [SerializeField] Paddle paddle1;
+    [SerializeField] float xPush = 2f;
+    [SerializeField] float yPush = 15f;
 
     Vector2 paddleToBallVector;
+    bool hasStarted = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +19,22 @@ public class Ball : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(!hasStarted){
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        }
+        
+    }
+
+    private void LockBallToPaddle(){
         Vector2 paddlePos = new Vector2(paddle1.transform.position.x,paddle1.transform.position.y);
         transform.position = paddlePos + paddleToBallVector;
+    }
+
+    private void LaunchOnMouseClick(){
+        if(Input.GetMouseButtonDown(0)){
+            hasStarted = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush,yPush);
+        }
     }
 }

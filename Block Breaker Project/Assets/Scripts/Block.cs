67820e246a96f4293cd6c6017c25b990d54e65db
david <davss0802@gmail.com>
2020-05-23 17:42:36 +0400

    Counting Breakable Blocks

--- Block Breaker Project/Assets/Scripts/Block.cs
+++ Block Breaker Project/Assets/Scripts/Block.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class Block : MonoBehaviour
 {
     [SerializeField] AudioClip breakSound;
+    Level level;
+
+      void Start()
+    {
+        level = FindObjectOfType<Level>();
+        level.CountBreakableBlocks();
+    }
     private void OnCollisionEnter2D(Collision2D other) {
         AudioSource.PlayClipAtPoint(breakSound,Camera.main.transform.position);
        Destroy(gameObject);
